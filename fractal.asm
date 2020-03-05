e35039fbc296dab7a044a4b0a6479b154d583889
Stanislav Maslovski <stanislav.maslovski@gmail.com>
2020-03-05 05:45:18 +0000

    184 bytes

--- fractal.asm
+++ fractal.asm
@@ -1,29 +1,29 @@
-	org	#6000
+	org	6000h
 ;**********************************
 ; Tabulate fixpoint squares with
 ; accuracy up to 2^-8
-; Formats of 16 bit data:
-; operand-index: 0000XXX.X XXXXXXX0
+; Formats of 16 bit data (S=sign):
+; operand-index: 1SSSXXX.X XXXXXXX0
 ; table result:  0XXXXXX.X XXXXXXX0
-;**********************************
-tbl	equ	#8000
 ;**********************************
 init:
 	di
-	ld	hl, tbl
-	ld	sp, hl
-	pop	de		; increment SP by 2
 	xor	a		; 24 bit accumulator in A:DE
 	ld	d, a
 	ld	e, a
+	ld	l, a
+	ld	h, a
+	ld	sp, hl
+	ld	h, 80h
+	pop	bc		; increment SP by 2
 i1:
 	ld	b, a
 	ld	c, d
 	res	0, c		; make it even
-	push	bc		; store BC to bottom half
-	ld	(hl), c		; and next to
-	inc	hl		;  upper
-	ld	(hl), b		;   half
+	push	bc		; store BC to high tbl
+	ld	(hl), c		; and to
+	inc	hl		;  low
+	ld	(hl), b		;   tbl
 	push	hl		; always below the data, can use
 	add	hl, hl		; HL = 2*x + 2^-8, bit15 = 0
 	add	hl, de
@@ -43,20 +43,6 @@ dy	equ	dx
 x0	equ	-170*dx
 y0	equ	-96*dx
 ;**********************************
-tab_bc	macro
-	add	hl, bc	;11
-	ld	c, (hl)	; 7
-	inc	hl	; 6
-	ld	b, (hl) ; 7
-	endm		; = 31 t
-;**********************************
-tab_de	macro
-	add	hl, de
-	ld	e, (hl)
-	inc	hl
-	ld	d, (hl)
-	endm
-;**********************************
 st_ix   macro   hi, lo
         ld      hi, ixh
         ld      lo, ixl
@@ -75,85 +61,78 @@ mndbrt:
 newbt:
 	ld	c, 1		; initial bit mask
 newpx:
+	ld	b, niter	; max iteration number
+
+	exx			; moved here, can be useful
+
 	ld	de, dx
 	add	ix, de		; update a
+	ld	a, ixh
+	or	#80		; fix MSB, because sign can change
+	ld	ixh, a
 	ld	(x), ix		; x = a
 	ld	(y), iy		; y = b
+	ld	sp, iy
+	jr	entry
 
-	ld	b, niter	; max iteration number
 loop:
 	exx
 
+	ld	d, h
+	ld	e, l		; DE = x^2+y^2
+
+	sbc	hl, bc		; here CF is not set
+	sbc	hl, bc		; HL = x^2-y^2
+	st_ix	b, c		; a
+	add	hl, bc		; HL = x^2-y^2+a
+	set	7, h		; fix MSB
+	ld	(x), hl		; new x
+
 y	equ	$+1		; imm16 as y variable
-	ld	bc, 0
-	ld	hl, tbl
-	tab_bc			; BC = y^2
+	ld	hl, 2121h
+	dec	sp
+	dec	sp		; old x
+	add	hl, sp		; HL = x+y
+	set	7, h		; fix MSB
+	ld	sp, hl
+	pop	hl		; HL = (x+y)^2
+
+	and	a		; clear CF
+	sbc	hl, de		; HL = 2*x*y
+	st_iy	d, e		; b
+	add	hl, de		; HL = 2*x*y+b
+	set	7, h
+	ld	(y), hl		; new y
+	ld	sp, hl
+entry:
+	pop	bc		; BC = y^2
 
 x	equ	$+1		; imm16 as x variable
-	ld	de, 0
-	ld	hl, tbl
-	tab_de			; DE = x^2
-
-	ex	de, hl
+	ld	sp, 3131h
+	pop	hl		; HL = x^2
 	add	hl, bc		; HL = x^2+y^2
 
 	ld	a, max2
 	cp	h		; H > max2 ?
+
+	exx
+
 	jr	c, ovfl
-
-	push	hl
-
-	sbc	hl, bc		; here CF is not set
-	sbc	hl, bc		; HL = x^2-y^2
-
-	pop	bc		; BC = x^2+y^2
-
-	push	hl
-
-	ld	hl, (x)
-	ld	de, (y)
-	add	hl, de
-	ld	de, tbl
-	tab_de			; DE = (x+y)^2
-
-	and	a		; clear CF
-	ex	de, hl
-	sbc	hl, bc
-	st_iy	d, e		; b
-	add	hl, de		; HL = 2*x*y+b
-	ld	(y), hl
-
-	pop	hl
-
-	st_ix	d, e		; a
-	add	hl, de		; HL = x^2-y^2+a
-	ld	(x), hl
-
-	exx
-
 	djnz	loop		; to next iteration
-	jr	norm
 ovfl:
-	exx
-norm:
 	rr	b
 	rl	c		; update bitmask
 	jr	nc, newpx	; to newpixel
 
 	ld	(hl), c		; write to upper half-screen
-
-	push	hl
 	ld	a, l
 	xor	0e0h
-	ld	l, a
+	ld	e, a
 	ld	a, 97h
 	sub	h
-	ld	h, a
-	ld	(hl), c		; write to lower half-screen
-	ld	de, 489fh	; check for end condition
-	sbc	hl, de
-	pop	hl
-	jp	z, mndbrt	; restart
+	ld	d, a
+	ld	a, c
+	ld	(de), a		; write to lower half-screen
 
 	inc	l
 	ld	a, 1fh
@@ -162,7 +141,8 @@ norm:
 
 	ld	ix, x0		; a = x0
 	ld	de, dy
-	add	iy, de		; update b
+	add	iy, de		; update b, no need to fix MSB
+	jp	c, mndbrt	; restart when iy >= 0
 
 	inc	h
 	ld	a, 07h
@@ -182,5 +162,5 @@ n1:
 	sub	08h
 	ld	h, a
 	jp	newbt
-	ret
+
 	end	init
