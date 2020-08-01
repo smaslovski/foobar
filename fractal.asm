128a21ba07f33f37131ce8d1404bf06f5d4aee21
lvd <lvd2@users.noreply.github.com>
2020-08-02 01:31:31 +0300

    taken stas's branch, 179->177 bytes

--- fractal.asm
+++ fractal.asm
@@ -1,29 +1,29 @@
-	org	#b000 ; on rubber speccies, this is FAST ram, so overall code runs somewhat faster
+	org	#c000
 ;**********************************
 ; Tabulate fixpoint squares with
 ; accuracy up to 2^-8
-; Formats of 16 bit data:
-; operand-index: 0000XXX.X XXXXXXX0
+; Formats of 16 bit data (S=sign):
+; operand-index: 1SSSXXX.X XXXXXXX0
 ; table result:  0XXXXXX.X XXXXXXX0
-;**********************************
-tbl	equ	#8000
 ;**********************************
 init:
 	di
-	ld	hl, tbl
-	ld	sp, hl
-	pop	de		; increment SP by 2
 	xor	a		; 24 bit accumulator in A:DE
 	ld	d, a
 	ld	e, a
+	ld	l, a
+	ld	h, a
+	ld	sp, hl
+	ld	h, 80h
+	pop	bc		; increment SP by 2
 i1:
 	ld	b, a
 	ld	c, d
 	res	0, c		; make it even
-	push	bc		; store BC to bottom half
-	ld	(hl), c		; and next to
-	inc	hl		;  upper
-	ld	(hl), b		;   half
+	push	bc		; store BC to high tbl
+	ld	(hl), c		; and to
+	inc	hl		;  low
+	ld	(hl), b		;   tbl
 	push	hl		; always below the data, can use
 	add	hl, hl		; HL = 2*x + 2^-8, bit15 = 0
 	add	hl, de
@@ -38,196 +38,131 @@ i1:
 ;**********************************
 niter	equ	32
 max2	equ	7
-;dx	equ	6
-;dy	equ	dx
-delta	equ	6
-x0	equ	-170*delta
-y0	equ	-96*delta
-;**********************************
-tab_bc	macro
-	add	hl, bc	;11
-	ld	c, (hl)	; 7
-	inc	l	; 4  ;low byte is always at even address
-	ld	b, (hl) ; 7
-	endm		; = 29 t
-;**********************************
-tab_de	macro
-	add	hl, de
-	ld	e, (hl)
-	inc	l
-	ld	d, (hl)
-	endm
-;**********************************
-st_ix   macro   hi, lo
-        ld      hi, ixh
-        ld      lo, ixl
-        endm
-;**********************************
-st_iy   macro   hi, lo
-        ld      hi, iyh
-        ld      lo, iyl
-        endm
+dx	equ	6
+dy	equ	dx
+x0	equ	-170*dx
+y0	equ	-96*dx
 ;**********************************
 
 mndbrt:
-	ld	de, delta
 	ld	hl, 4000h	; screen start addr
-	ld	ix, x0		; a = x0
-	ld	iy, y0		; b = y0
+newln:
+	ld	d,h
+	ld	e,l
 newbt:
-	ld	(hl),1		; initial bit mask
+	ld	(hl), 1		; initial bit mask
 newpx:
-	add	ix, de		; update a
-	ld	(x), ix		; x = a
-	ld	(y), iy		; y = b
-
 	ld	b, niter	; max iteration number
+
+	exx			; moved here, can be useful
+
+	ld	de, dx
+	ld	hl, (za)
+	add	hl, de		; update a
+	set	7, h		; fix MSB, because sign can change
+	ld	(za), hl
+	ld	(x), hl		; x = a
+	ld	hl, (zb)
+	ld	(y), hl		; y = b
+	jr	entry
+
 loop:
 	exx
 
+	ld	d, h
+	ld	e, l		; DE = x^2+y^2
+
+	sbc	hl, bc		; here CF is not set
+	sbc	hl, bc		; HL = x^2-y^2
+za	equ	$+1
+	ld	bc, x0		; a
+	add	hl, bc		; HL = x^2-y^2+a
+	set	7, h		; fix MSB
+	ld	(x), hl		; new x
+
 y	equ	$+1		; imm16 as y variable
-	
-	;;;ld	bc, 0
-	;;;ld	hl, tbl
-	;;;tab_bc			; BC = y^2
+	ld	hl, 2121h
+	dec	sp
+	dec	sp		; old x
+	add	hl, sp		; HL = x+y
+	set	7, h		; fix MSB
+	ld	sp, hl
+	pop	hl		; HL = (x+y)^2
 
-	ld	hl,#2121  ;profit if we would pack the executable
-	ld	a,#80     ;this inversion could be incorporated in the table and coordinates?
-		          ;it does not affect subtraction nor addition
-	xor	h
-	ld	h,a       ;same bytes, but 25 tc instead of 31 tc
-	ld	c,(hl)
-	inc	l
-	ld	b,(hl)
-
+	and	a		; clear CF
+	sbc	hl, de		; HL = 2*x*y
+zb	equ	$+1
+	ld	de, y0		; b
+	add	hl, de		; HL = 2*x*y+b
+	set	7, h
+	ld	(y), hl		; new y
+entry:
+	ld	sp, hl
+	pop	bc		; BC = y^2
 
 x	equ	$+1		; imm16 as x variable
-
-	;;;ld	de, 0
-	;;;ld	hl, tbl
-	;;;tab_de			; DE = x^2
-	ld	hl,#2121
-	ld	a,#80
-	xor	h
-	ld	h,a
-	ld	e,(hl)
-	inc	l
-	ld	d,(hl)
-
-	ex	de, hl
+	ld	sp, 3131h
+	pop	hl		; HL = x^2
 	add	hl, bc		; HL = x^2+y^2
 
 	ld	a, max2
 	cp	h		; H > max2 ?
+
+	exx
+
 	jr	c, ovfl
-
-	push	hl
-
-	sbc	hl, bc		; here CF is not set
-	sbc	hl, bc		; HL = x^2-y^2
-
-	pop	bc		; BC = x^2+y^2
-
-	push	hl
-
-	ld	hl, (x)
-	ld	de, (y)
-	add	hl, de
-
-	;;;ld	de, tbl
-	;;;tab_de			; DE = (x+y)^2
-	ld	a,#80
-	xor	h
-	ld	h,a
-	ld	e,(hl)
-	inc	l
-	ld	d,(hl)
-
-	;;;;and	a		; clear CF -- XOR H has already cleared CY!
-	ex	de, hl
-	sbc	hl, bc
-
-	;;;st_iy	d, e		; b
-	push	iy
-	pop	de	;1 byte smaller
-
-	add	hl, de		; HL = 2*x*y+b
-	ld	(y), hl
-
-	pop	hl
-
-	;;;st_ix	d, e		; a
-	push	ix
-	pop	de	;1 byte smaller
-
-	add	hl, de		; HL = x^2-y^2+a
-	ld	(x), hl
-
-	exx
-
 	djnz	loop		; to next iteration
-	;;;jr	norm
-	db	#3E	;opcode for LD A,imm
 ovfl:
-	exx
-norm:
 	rr	b
-	rl	(hl)		; update bitmask
-	jr	nc, newpx	; to newpixel
+	rl	(hl)		; update bitmask in place
+	jr	nc, newpx	; to new pixel
 
-	;;;ld	(hl), c		; write to upper half-screen
-
-;;;	push	hl
 	ld	a, l
 	xor	0e0h
 	ld	c, a
 	ld	a, 97h
 	sub	h
 	ld	b, a
-	;;;ld	(hl), c		; write to lower half-screen
-	ld	a,(hl)
-	ld	(bc),a
-
-;;;	ld	de,489fh	; check for end condition
-;;;	sbc	hl, de
-;;;	pop	hl
-
-;;;	jr	z, mndbrt	; restart
+	ld	a, (hl)
+	ld	(bc), a		; write to lower half-screen
 
 	inc	l
-	ld	a, 1fh
-	and	l
-newbt3:	jr	nz,newbt
+	ld	a,l
+	and	31
+	jr	nz, newbt
 
-	ld	ix, x0		; a = x0
-	add	iy, de		; update b
+	exx
+	ld	hl, x0
+	ld	(za), hl	; set a = x0
+	ld	de, dy
+	ld	hl, (zb)
+	add	hl, de		; update b, no need to fix MSB
+	ld	(zb), hl
+	exx
+trap:
+	jr	c, trap		; loop when iy >= 0
+
+	
+	ex	de,hl
 
 	inc	h
-	ld	a, 07h
-	and	h
-	jr	z, n1
+	ld	a,h
+	and	7
+	jr	nz,newln2
+	ld	a,l
+	add	a,32
+	ld	l,a
+	jr	c,newln2
+	ld	a,h
+	sub	8
+	ld	h,a
+newln2
+	jp	newln
 
-	ld	a, l
-	sub	20h
-	ld	l, a
-;newbt2:	jr	newbt2
-	jr	newbt2
-n1:
-	ld	a, 0e0h
-	and	l
-	jr	z,newbt2
 
-	ld	a, h
-	sub	08h
-	ld	h, a
-
-	xor	l
-newbt2:	cp	#c8 ;here only when H=40 and L=00,20,40,60,80,A0,C0,E0
-		    ; or H=48 and L=00,20,40,60,80. 4880 is exit condition
-	jr	nz,newbt3
-	halt	;stops the cpu because of previous DI
-
-	;;;jp	newbt
-	;;;ret
 	end	init
+
+
+
+
 
